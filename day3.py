ac140f10e9fcdd018f3c6ccb13fe71e5cdae8350
burrches <bryjeannotte@gmail.com>
2022-01-02 14:33:20 -0500

    ignore swap files and add day3 part2

--- day3.py
+++ day3.py
@@ -13,12 +13,14 @@ epsilon is least common bits of all power readings, or inverse of gamma
 
 def read_input(f_name: str = "day3.input") -> List[str]:
     """
+    read and format data from day3.input
     """
-    with open(f_name, "r") as f:
-        return [line.strip() for line in f.readlines()]
+    with open(f_name, "r") as file_:
+        return [line.strip() for line in file_.readlines()]
 
 def part1():
     """
+    part 1 of day 3 solution
     """
     nums = read_input()
     gamma = ''
@@ -44,11 +46,13 @@ def part1():
     print("epsilon:", epsilon, "decimal:", int(epsilon, base=2))
     return int(gamma, base=2) * int(epsilon, base=2)
 
-def oxy_bit_criteria(nums, index, bit):
-    ret = []
-    for num in nums:
-        pass
-
+def bit_criteria(nums, index, bit):
+    """
+    nums - ([str]) Numbers search through
+    index - (int) The index in each num of nums to consider
+    bit - (str) The bit to compare at num[index]
+    """
+    return [num for num in nums if num[index] == bit]
 
 def part2():
     """
@@ -67,26 +71,52 @@ def part2():
         C02: least common value in current bit position. if 0/1 equally
         common, keep values with a 0 in the position being considered
     """
-    #nums = read_input()
-    nums = ['00100', '11110', '10110', '10111', '10101', '01111', '00111',
-            '11100', '10000', '11001', '00010', '01010',]
+    nums = read_input()
+    #nums = ['00100', '11110', '10110', '10111', '10101', '01111', '00111',
+    #        '11100', '10000', '11001', '00010', '01010',]
 
     # Find most common bits in each position
     bits = [0 for i in nums[0]]
-    #print(bits)
-
-    for num in nums:
-        #print(num)
-        for index, bit in enumerate(num):
-            #print(index, bit)
-            bits[index] += int(bit)
-
-    for place in bits:
-        gamma = f"{gamma}1" if place >= len(nums) / 2 else f"{gamma}0"
-
-    epsilon = ''.join(['1' if bit == '0' else '0' for bit in gamma])
 
     # Now, begin finding oxygen generation ratings(23) and c02 (10)
+    tmp = list(nums)
+    for index, _ in enumerate(bits):
+        zeros = ones = 0
+        for num in tmp:
+            if num[index] == '0':
+                zeros += 1
+            else:
+                ones += 1
+
+        if ones >= zeros:
+            tmp = bit_criteria(tmp, index, '1')
+        else:
+            tmp = bit_criteria(tmp, index, '0')
+
+        if len(tmp) == 1:
+            oxy_rating = tmp[0]
+            print(oxy_rating)
+            break
+
+    tmp = list(nums)
+    for index, _ in enumerate(bits):
+        zeros = ones = 0
+        for num in tmp:
+            if num[index] == '0':
+                zeros += 1
+            else:
+                ones += 1
+
+        if zeros > ones:
+            tmp = bit_criteria(tmp, index, '1')
+        else:
+            tmp = bit_criteria(tmp, index, '0')
+        if len(tmp) == 1:
+            c02_rating = tmp[0]
+            print(c02_rating)
+            break
+
+    return int(oxy_rating, base=2) * int(c02_rating, base=2)
 
 if __name__ == "__main__":
     print("part1 answer:", part1())
